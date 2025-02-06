77a6107d9b1ab57625f9f0808e0617822736ef7e
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2025-02-05 20:33:53 -0500

    Removed the callback url parameter entirely

--- index.js
+++ index.js
@@ -82,7 +82,6 @@ passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     // callbackURL: "http://localhost:3000/auth/github/callback"
-    callbackURL: "https://a3-alexanderbeck-sa.onrender.com/auth/github/callback"
 },
     async function (accessToken, refreshToken, profile, done) {
         debugPrint("Successfully connected to Github");
